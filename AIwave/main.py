bc96ed77d84ccc1d4283039580ca3833214bd324
Phuong Phung <pkp013@gmail.com>
2022-07-25 16:29:14 +0700

    Update main.py

--- AIwave/main.py
+++ AIwave/main.py
@@ -2,19 +2,32 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
-df = pd.read_csv(
-  "data_ann.csv"
-)
-
+from io import StringIO
 st.title("Nghiên cứu mạng nơ ron nhân tạo ANN dự đoán tham số sóng gần bờ khu vực Cửa Đại tỉnh Quảng Ngãi")
 
-st.write("Du lieu:") 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
 
-fig = px.line(        
-        df, #Data Frame
-        x = [i for i in range(8738)], #Columns from the data frame
-        y = "Gần bờ_Mike",
-        title = "Line frame"
-    )
-st.plotly_chart(fig)
-st.write(df)
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+    fig = px.line(        
+            dataframe, #Data Frame
+            x = [i for i in range(8738)], #Columns from the data frame
+            y = "Gần bờ_Mike",
+            title = "Line frame"
+        )
+    st.plotly_chart(fig)
+    st.write(dataframe)
